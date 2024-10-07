fc1c8c278422b3be58989d76ff09617a7968e83d
Joaquin <joaquin.oterod@gmail.com>
2024-10-07 22:31:44 +0000

    ADD. Se termina el punto C

--- RTA_SCRIPT_Examen_20241006/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_C.sh
@@ -9,10 +9,12 @@ sudo chown p1c2_2024_A2:p1c2_2024_A2 /alumno_2/
 sudo chown p1c2_2024_A3:p1c2_2024_A3 /alumno_3/
 sudo chown p1c2_2024_A1:p1c2_2024_gProfesores /profesores/
 
-sudo chmod 750 /alumno_1
-sudo chmod 760 /alumno_2
-sudo chmod 700 /alumno_3
-sudo chmod 775 /profesores
+sudo chmod 750 /Examenes-UTN/alumno_1
+sudo chmod 760 /Examenes-UTN/alumno_2
+sudo chmod 700 /Examenes-UTN/alumno_3
+sudo chmod 775 /Examenes-UTN/profesores
 
 sudo su -c "whoami > /Examenes-UTN/alumnos_1/validar1.txt" p1c2_2024_A1
-
+sudo su -c "whoami > /Examenes-UTN/alumnos_2/validar1.txt" p1c2_2024_A2
+sudo su -c "whoami > /Examenes-UTN/alumnos_3/validar1.txt" p1c2_2024_A3
+sudo su -c "whoami > /Examenes-UTN/profesores/validar1.txt" p1c2_2024_P1
