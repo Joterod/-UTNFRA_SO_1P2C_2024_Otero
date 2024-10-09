f833931cc78a96b716a05ca71c983c58a60742bf
Joaquin <joaquin.oterod@gmail.com>
2024-10-09 21:47:41 +0000

    FIX. Se corrige el error de la carpeta cliente aclarado en fe de erratas}

--- RTA_SCRIPT_Examen_20241006/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -1,1 +1,1 @@
-mkdir -p ~/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},clientes/cartas_{1..100}}
+mkdir -p ~/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},cliente/cartas_{1..100}}
