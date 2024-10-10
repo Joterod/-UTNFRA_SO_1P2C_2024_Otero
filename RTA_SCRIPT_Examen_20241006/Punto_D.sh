5f8e744216f90c3403a669ad03e268cedeecb773
Joaquin <joaquin.oterod@gmail.com>
2024-10-10 22:25:09 +0000

    FIX. Se corrije error que hacia que se genere el archivo en la carpeta del usuario root

--- RTA_SCRIPT_Examen_20241006/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -1,1 +1,3 @@
-sudo bash -c 'mkdir -p ~/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},cliente/cartas_{1..100}}'
+#!/bin/bash
+
+bash -c 'mkdir -p ~/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},cliente/cartas_{1..100}}'
