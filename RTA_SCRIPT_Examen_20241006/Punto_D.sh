33791a23a3ba12efd93fd7b6d2b8d46e4f2f5a8e
Joaquin <joaquin.oterod@gmail.com>
2024-10-10 03:44:20 +0000

    Fix. Se agregan correcciones de ultimo momento

--- RTA_SCRIPT_Examen_20241006/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -1,1 +1,1 @@
-mkdir -p ~/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},cliente/cartas_{1..100}}
+sudo bash -c 'mkdir -p ~/Estructura_Asimetrica/{correo/{cartas_{1..100},carteros_{1..10}},cliente/cartas_{1..100}}'
